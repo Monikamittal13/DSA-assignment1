39866903ac21fe1d6707a0935848aff439129fa5
Monikamittal13 <110057510+Monikamittal13@users.noreply.github.com>
2022-10-12 00:26:43 +0530

    Update question3.js

--- question3.js
+++ question3.js
@@ -1,17 +1,16 @@
 //Q3. Write a program to check if two strings are a rotation of each other?
 
-const inp1 = "hello",
-  inp2 = "olleh";
-if (inp1.length == inp2.length) {
-  let i;
-  for (i = 0; i < inp1.length; i++) {
-    if (inp1[i] != inp2[inp2.length - 1 - i]) {
+const str1 = "hello",
+  str2 = "olleh";
+if (str1.length == str2.length) {
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] != str2[str2.length - 1 - i]) {
       console.log("Given Two Strings are a Not Rotation of Each Other");
     }
   }
-  if (i == inp2.length) {
+  if (let i == str2.length) {
     console.log("Given Two Strings are The Rotation of Each Other");
   }
 } else {
   console.log("Given Two Strings are a Not Rotation of Each Other");
-}
+ }

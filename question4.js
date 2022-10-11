2940fee8c7cbcb04225b85464f5deefbeb7b76e8
Monikamittal13 <110057510+Monikamittal13@users.noreply.github.com>
2022-10-12 00:27:27 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,24 +1,24 @@
 //Q4. Write a program to print the first non-repeated character from a string?
 
-function find_FirstNotRepeatedChar(str) {
-    var arra1 = str.split("");
-    var result = "";
-    var ctr = 0;
-  
-    for (var x = 0; x < arra1.length; x++) {
-      ctr = 0;
-  
-      for (var y = 0; y < arra1.length; y++) {
-        if (arra1[x] === arra1[y]) {
-          ctr += 1;
-        }
-      }
-  
-      if (ctr < 2) {
-        result = arra1[x];
-        break;
+function FirstNotRepeated(str) {
+  var arr1 = str.split("");
+  var result = "";
+  var char = 0;
+
+  for (var x = 0; x < arr1.length; x++) {
+    char = 0;
+
+    for (var y = 0; y < arr1.length; y++) {
+      if (arr1[x] === arr1[y]) {
+        char += 1;
       }
     }
-    return result;
+
+    if (char < 2) {
+      result = arr1[x];
+      break;
+    }
   }
-  console.log(find_FirstNotRepeatedChar("abacddbec"));
+  return result;
+}
+console.log(FirstNotRepeated("abacddbec"));

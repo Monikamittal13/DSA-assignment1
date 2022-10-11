1d57aacbccf2f567c49a36e0ae0184c030972125
Monikamittal13 <110057510+Monikamittal13@users.noreply.github.com>
2022-10-12 00:31:22 +0530

    Update question8.js

--- question8.js
+++ question8.js
@@ -1,42 +1,41 @@
 //Q8. Write a program to check if all the brackets are closed in a given code snippet.
 
-function areBracketsBalanced(expr) {
-    let stack = [];
-  
-    for (let i = 0; i < expr.length; i++) {
-      let x = expr[i];
-  
-      if (x == "(" || x == "[" || x == "{") {
-        stack.push(x);
-        continue;
-      }
-  
-      if (stack.length == 0) return false;
-  
-      let check;
-      switch (x) {
-        case ")":
-          check = stack.pop();
-          if (check == "{" || check == "[") return false;
-          break;
-  
-        case "}":
-          check = stack.pop();
-          if (check == "(" || check == "[") return false;
-          break;
-  
-        case "]":
-          check = stack.pop();
-          if (check == "(" || check == "{") return false;
-          break;
-      }
+function isBalanced(exp) {
+  let stack = [];
+
+  for (let i = 0; i < exp.length; i++) {
+    let x = exp[i];
+
+    if (x == "(" || x == "[" || x == "{") {
+      stack.push(x);
+      continue;
     }
-  
-    return stack.length == 0;
+
+    if (stack.length == 0) return false;
+
+    let check;
+    switch (x) {
+      case ")":
+        check = stack.pop();
+        if (check == "{" || check == "[") return false;
+        break;
+
+      case "}":
+        check = stack.pop();
+        if (check == "(" || check == "[") return false;
+        break;
+
+      case "]":
+        check = stack.pop();
+        if (check == "(" || check == "{") return false;
+        break;
+    }
   }
-  
-  let expr = "([{}])";
-  
-  if (areBracketsBalanced(expr)) console.log("Balanced ");
-  else console.log("Not Balanced ");
-  
+
+  return stack.length == 0;
+}
+
+let exp = "([{}])";
+
+if (isBalanced(exp)) console.log("Balanced ");
+else console.log("Not Balanced ");
